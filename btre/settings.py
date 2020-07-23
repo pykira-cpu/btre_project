9864029e4d4e1de15a0c1b1533ccc076777ee68b
nsn <47124992+nsnakhil@users.noreply.github.com>
2020-07-23 19:03:37 -0400

    update settings

--- btre/settings.py
+++ btre/settings.py
@@ -82,9 +82,9 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'btredb',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres2020',
+        'NAME': '------',
+        'USER': '-------',
+        'PASSWORD': '------',
         'HOST': 'localhost'
     }
 }
