15bbe99f7c69dd46bbd3987252aa3f1b835e7232
nsn <sainagaakhil@gmail.com>
2020-07-23 18:41:30 -0400

    initial commit

---
+++ pages/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from listings.choices import state_choices,bedroom_choices,price_choices
+
+from listings.models import Listing
+from realtors.models import Realtor
+
+def index(request):
+    #Get 3 listings
+    listings = Listing.objects.order_by('-list_date').filter(is_published = True)[:3]
+
+    context = { 'listings':listings,
+                'state_choices': state_choices,
+                'bedroom_choices': bedroom_choices,
+                'price_choices': price_choices
+                }
+    
+    return render(request,'pages/index.html', context)
+
+def about(request):
+    #Get all realtors
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    #Get mvp
+    mvp_realtors = Realtor.objects.all().filter(is_mvp = True)
+
+    context = {
+        'realtors':realtors,
+        'mvp_realtors':mvp_realtors
+
+    }
+    return render(request,'pages/about.html', context)
